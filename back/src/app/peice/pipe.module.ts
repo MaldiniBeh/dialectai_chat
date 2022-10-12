d757ff5a800a2b960b65171d27c6bcf30031fa8d
Behanzin Fred <freddybehan713@gmail.com>
2022-10-12 14:19:43 +0100

    refactoring

--- back/src/app/peice/pipe.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { PricePipe } from "./price.pipe";
-
-@NgModule({
-  declarations: [PricePipe],
-  imports: [],
-  exports: [PricePipe]
-})
-export class PipesModule { }
